026dfd0af4deec6171e33f4fee70c4690f9d1a39
toby-tan-ics4u1-02-25 <toby.tan@student.tdsb.on.ca>
2025-04-09 00:05:34 +0000

    SKADOOSH

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,21 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+	public static int sumOfDigits(int num){
+		if(num < 10 && num > -10){
+			return Math.abs(num);
+		}
+		return Math.abs(num % 10) + sumOfDigits(num / 10);
+	}
+	public static int factorial(int SKABOOM){
+		if(SKABOOM == 0){
+			return 1;
+		}
+		else if(SKABOOM < 0){
+			return 0;
+		}
+		return SKABOOM * factorial(SKABOOM - 1);
+	}
 	
 
 }
